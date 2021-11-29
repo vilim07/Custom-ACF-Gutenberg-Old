af5a9aae84330a2fe150062d7cdedd6f0d0249fe
vilim07 <vrapic@gmail.com>
2021-11-29 14:12:09 +0100

    888

--- template-parts/people-repeater.php
+++ template-parts/people-repeater.php
@@ -3,3 +3,16 @@
     $content = $attributes["content"];
 ?>
 
+
+<section>
+    <div class="container">
+        <div class="row my-5">
+        <?php 
+        foreach ($content as $osoba){?>
+            <div class="col-3">
+                <h1><?php echo $osoba["ime"]; ?></h1>
+            </div>
+        <?php }?>
+        </div>
+    </div>
+</section>
